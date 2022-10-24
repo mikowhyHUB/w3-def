80784b41984c45e191f75e59bfebc61ad00cfd56
itsnotmikowhy <111461945+itsnotmikowhy@users.noreply.github.com>
2022-10-24 18:44:53 +0200

    more exercises

---
+++ list_float_num.py
@@ -0,0 +1,7 @@
+'''
+Accept a list of 5 float numbers as an input from the user
+'''
+
+out = list(float(int(input(': ').split(','))))
+
+print(out)
---
+++ three_string_in_one.py
@@ -0,0 +1,7 @@
+'''
+Write a program to take three names as input from a user in the single input() function call.
+'''
+
+a, b, c = input(': ').split(' ')
+
+print(a, b, c)
